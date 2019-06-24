e0d5c3b5d724adaf4226f0531415bb20e9661fde
Zach <z61@zeesixtyone.com>
2019-06-23 20:56:42 -0400

    indent

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -34,9 +34,9 @@
                 <!-- Authentication Links -->
                 @guest
                     <li><a href="{{ route('login') }}">Login</a></li>
-                @if(env('REGISTER_ENABLED'))
+                    @if(env('REGISTER_ENABLED'))
                     <li><a href="{{ route('register') }}">Register</a></li>
-                @endif
+                    @endif
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
