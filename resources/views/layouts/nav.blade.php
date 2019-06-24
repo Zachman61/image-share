ee2ab8b35fc54ff2bb204e0d6ca908f4fbb3fa04
Zach <z61@zeesixtyone.com>
2019-06-23 20:38:07 -0400

    fix broken link

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -34,7 +34,9 @@
                 <!-- Authentication Links -->
                 @guest
                     <li><a href="{{ route('login') }}">Login</a></li>
+                @if(env('REGISTER_ENABLED'))
                     <li><a href="{{ route('register') }}">Register</a></li>
+                @endif
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
