c643c5c58d277f4525db4da2ac949275a2dc3e7d
Zach <z61@zeesixtyone.com>
2020-03-11 00:16:22 -0400

    Add user model signed_in_at

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'username', 'email', 'password'
+        'username', 'email', 'password', 'signed_in_at'
     ];
 
     /**
@@ -26,6 +26,10 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 'remember_token',
+    ];
+
+    protected $dates = [
+        'signed_in_at'
     ];
 
     public function images()
